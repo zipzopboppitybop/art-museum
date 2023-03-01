898c89882b055775fb1f13d9e1d1c10c6825ee77
Natalie <cnapeel@gmail.com>
2023-02-28 17:28:52 -0700

    Very beginning complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import harvardArt from "./data/harvardArt";
+import GalleryNavigation from "./components/GalleryNavigation/index.js";
+
 function App() {
+  // const galleries = harvardArt.records;
   return (
-    <h1>Hello from App</h1>
-  );
+  <GalleryNavigation galleries={harvardArt.records} /> 
+  ) 
 }
 
 export default App;
---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+function GalleryNavigation (props) {
+    console.log(props);
+  return (
+    <h1>Galleries</h1>
+  )
+}
+
+export default GalleryNavigation;
